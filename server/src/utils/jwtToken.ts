dfcc48ca82a253c7217e0d8130f63b575b4d753d
sharafath11 <abisharfath@gmail.com>
2025-10-28 23:06:36 +0530

    bacend cookies issues testing 2

--- server/src/utils/jwtToken.ts
+++ server/src/utils/jwtToken.ts
@@ -13,22 +13,22 @@ export interface TokenPayload {
   role: string;
 }
 
-const cookieOptions = {
-  httpOnly: true,
-  secure: process.env.COOKIE_SECURE === "true",
-  sameSite: process.env.COOKIE_SAMESITE || "none",
-  domain: process.env.COOKIE_DOMAIN || ".learnvista.com",
-  path: "/",
-};
-
-
 // const cookieOptions = {
 //   httpOnly: true,
-//   secure: process.env.NODE_ENV === "production", 
-//   sameSite: "none" as const,                    
-//   domain: ".sharafathabi.cloud", 
-//   path: "/",                                     
+//   secure: process.env.COOKIE_SECURE === "true",
+//   sameSite: process.env.COOKIE_SAMESITE || "none",
+//   domain: process.env.COOKIE_DOMAIN || ".learnvista.com",
+//   path: "/",
 // };
+
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", 
+  sameSite: "none" as const,                    
+  domain: ".sharafathabi.cloud", 
+  path: "/",                                     
+};
 
 export const generateAccessToken = (id: string, role: string): string => {
   const payload: TokenPayload = { id, role };
